e3442cd6e38e0e3d2e3b629b48a19f1f9cd5f5ec
Natalia Veras <verasnatali4@gmail.com>
2024-02-25 18:53:50 +0100

    Ripristino view data

--- src/js/Views/View.js
+++ src/js/Views/View.js
@@ -14,7 +14,7 @@ export default class View {
         if (type === "radio") {
           if (checked) acc[name] = value;
         } else if (type === "checkbox") {
-          acc[name] = value;
+          acc[name] = !!checked;
         } else {
           acc[name] = value;
         }
